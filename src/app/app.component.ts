6de110021d7b57a622b09bb013bc1d7ca609b3a5
yeysonsanchez <deflcox@gmail.com>
2020-05-03 13:07:37 -0500

    se implemento localStorage y DoCheck para insertar email de contacto en la barra de navegacion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component,DoCheck,OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements DoCheck,OnInit {
   title = 'curso-angular4';
+  //(Pso1) mostar contacto crear emailContacto
+  emailContacto: string;
+
+  ngDoCheck(){
+    //console.log('El DoCheck se ha ejecuadp')
+    //motrar contacto 
+    //(pso4) cada vez que cambiemos el contacto este se actualice en la barra de navegacion 
+    console.log('El DoCheck se ha ejecuadp')
+    this.emailContacto =localStorage.getItem('emailContacto');
+  }
+  ngOnInit(){
+    //esto permitira que cuando naveguemos en nustras pestañas podremos visylizar nuestro email. !claro en consola 
+    //console.log(localStorage.getItem('emailContacto'));
+    //(Pso2)mostrar contacto asignar el valor que tiene getItem 
+   this.emailContacto =localStorage.getItem('emailContacto');
+
+  }
 }
--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -7,7 +7,17 @@ import { Component,OnInit} from '@angular/core';
 })
 export class ContactComponent implements OnInit{
   title = 'Contacto';
+  emailContacto: string;
   ngOnInit (){
+  
       console.log('contact.coponent cargado!!');
+  }
+  guardarEmail(){
+    //validamos que nos este guardando nuestro correo con console.log
+    //console.log(this.emailContacto);
+    // guardar en el navegador LocalStorage ejemplo como si fueramos a guardar una sesion ene el navegador 
+    localStorage.setItem('emailContacto',this.emailContacto);
+    console.log(localStorage.getItem('emailContacto'));
+
   }
 }
