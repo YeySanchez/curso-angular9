6b98f7db83282c75ffba62f5bcaa1c1f1fe433cc
yeysonsanchez <deflcox@gmail.com>
2020-05-03 11:09:15 -0500

    comunicacion entre componentes de hijo a padre propiedad outputs

--- src/app/components/tienda/tienda.component.ts
+++ src/app/components/tienda/tienda.component.ts
@@ -9,11 +9,16 @@ import { Component } from '@angular/core';
   export class TiendaComponet {
     public titulo: string;
     public NombreDelParque: string;
-
+    public Miparque;
     constructor(){
         this.titulo = 'esta es la tienda';
     }
     MostrarNombre(){
       console.log(this.NombreDelParque);
+    }
+    verDatosParque(event){
+      console.log(event);
+      this.Miparque = event;
+
     }
   }
