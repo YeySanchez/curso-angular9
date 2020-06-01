aa3f0c43ae26a1e032050e9ee826337ccdd86e2c
yeysonsanchez <deflcox@gmail.com>
2020-05-31 21:59:55 -0500

    se añadio boton Mostrar texto junto al parrago con propiedades jquery

--- src/app/components/tienda/tienda.component.ts
+++ src/app/components/tienda/tienda.component.ts
@@ -1,17 +1,25 @@
-import { Component } from '@angular/core';
-
+import { Component,OnInit } from '@angular/core';
+declare var jQuery:any;
+declare var $:any;
 @Component({
     selector:'tienda',
     
     templateUrl: './tienda.component.html',
     styleUrls: ['./tienda.component.css'],
   })
-  export class TiendaComponet {
+  export class TiendaComponet implements OnInit {
     public titulo: string;
     public NombreDelParque: string;
     public Miparque;
     constructor(){
         this.titulo = 'esta es la tienda';
+    }
+    ngOnInit(){
+      $('#textojq').hide();
+      $('#botonjq').click(function(){
+        console.log('click desde jquery')
+        $('#textojq').slideToggle();
+      });
     }
     MostrarNombre(){
       console.log(this.NombreDelParque);
