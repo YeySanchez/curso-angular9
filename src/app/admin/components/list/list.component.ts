5b90fe59f3a23c8ee263b38cb648ead11026613b
yeysonsanchez <deflcox@gmail.com>
2020-07-08 20:39:46 -0500

    se ñadio lista de componentes en el panel control

--- src/app/admin/components/list/list.component.ts
+++ src/app/admin/components/list/list.component.ts
@@ -7,5 +7,9 @@ import { Component,DoCheck,OnInit } from '@angular/core';
 })
 export class ListComponent  {
   title = 'listado';
-  
+  //array manual//
+ //numbers =[0,1,2,3,4]//
+ //array automatico//
+   numbers = new Array(10);
+
 }
