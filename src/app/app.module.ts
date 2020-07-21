79580e0bc4be1e350bbdd78d562cd9c783c5b0dd
yeysonsanchez <deflcox@gmail.com>
2020-07-20 21:28:27 -0500

    boton con transiciones simples css3 en angular v35

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import{routing, appRoutingProviders} from'./app.routing';
-
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations'
 // IMPORTAR MODULO ModuloEmailModule//
 import {AdminModule} from './admin/admin.module';
 import {ModuloEmailMoule} from './moduloemail/moduloemail.module'
+
 
 import { AppComponent } from './app.component';
 import {TiendaComponet} from './components/tienda/tienda.component';
@@ -14,6 +15,7 @@ import {AnimalsComponent} from './components/animals/animals.component' ;
 import {ContactComponent} from './components/contact/contact.component';
 import {HomeComponent} from './components/home/home.component';
 import{KeepersComponent} from './components/keepers/keepers.component';
+import { from } from 'rxjs';
 
 
 
@@ -31,8 +33,10 @@ import{KeepersComponent} from './components/keepers/keepers.component';
     BrowserModule,
     FormsModule,
     routing,
+    BrowserAnimationsModule,
     ModuloEmailMoule,
     AdminModule
+    
   ],
   providers: [
     appRoutingProviders
--- src/app/components/tienda/tienda.component.ts
+++ src/app/components/tienda/tienda.component.ts
@@ -1,26 +1,60 @@
 import { Component,OnInit } from '@angular/core';
-declare var jQuery:any;
-declare var $:any;
+//importar librerarias para crerar transiciones en angular v35
+import { trigger, state, style, transition, animate} from '@angular/animations';
+
+//declare var jQuery:any;
+//declare var $:any;
 @Component({
-    selector:'tienda',
-    
+    selector:'tienda', 
     templateUrl: './tienda.component.html',
     styleUrls: ['./tienda.component.css'],
+    //crear array para crear tansformaciones con angular y css v35
+    animations: [
+      trigger('marcar',[
+
+          state('inactive', style({
+            border: '5px solid #ccc'
+
+          })),
+          state('active',style({
+            border: '5px solid yellow',
+            background: 'blue',
+            borderRadius: '50px',
+            transform: 'scale(1.2)'
+          })),
+          transition('inactive => active',[animate('3s linear')]),
+          transition('active => inactive',[animate('3s linear')])
+      ])
+
+    ]
+    //fin del array
   })
   export class TiendaComponet implements OnInit {
     public titulo: string;
     public NombreDelParque: string;
     public Miparque;
+    //crear variable estatus para poder convertir las transiciones de los  estados del boton v35
+    public status;
+
     constructor(){
         this.titulo = 'esta es la tienda';
+        this.status = 'inactive';
+     //crerar metodo para cambiar las transiciones de los estados del btono v35   
+    }
+    CambiarEstado(status){
+      if(status == 'inactive') {
+        this.status= 'active';
+      }else{
+        this.status= 'inactive';
+      }
     }
     ngOnInit(){
-      $('#textojq').hide();
-      $('#botonjq').click(function(){
-        console.log('click desde jquery')
-        $('#textojq').slideToggle();
-      });
-      $("#caja").dotdotdot({});
+     // $('#textojq').hide();
+      //$('#botonjq').click(function(){
+        //console.log('click desde jquery')
+      // $('#textojq').slideToggle();
+     // });
+      //$("#caja").dotdotdot({});
     }
     MostrarNombre(){
       console.log(this.NombreDelParque);
